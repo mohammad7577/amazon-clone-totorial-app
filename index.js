a389d5ff0f114abcb5059e44660b63e1f1704f61
unknown <hodrojmhmd288@gmail.com>
2022-10-23 22:17:46 +0300

    uploading server

---
+++ index.js
@@ -0,0 +1,38 @@
+// Imports from packages
+const express = require('express');
+const mongoose = require('mongoose');
+const adminRouter = require('./routes/admin');
+
+// IMPORTS FROM OTHER FILES
+const authRouter = require('./routes/auth');
+const productRoute = require('./routes/products');
+const userRouter = require('./routes/user');
+
+// INitialzations
+const PORT = 3000;
+const app = express();
+const DB = "mongodb+srv://mohammad:M1233210-9M@cluster0.bingc2g.mongodb.net/?retryWrites=true&w=majority";
+
+// middleware
+app.use(express.json());
+app.use(authRouter);
+app.use(adminRouter);
+app.use(productRoute);
+app.use(userRouter);
+
+// Connections
+mongoose
+    .connect(DB)
+    .then(() => {
+        console.log('Connection Successfully');
+    })
+    .catch((e) => {
+        console.log(e);
+    });
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`connected at port ${PORT}`);
+});
+
+
+
